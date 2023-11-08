2615131f1f04631fb0349bf6082adff83bb19851
alejox <alejoxdrums@gmail.com>
2023-11-08 17:15:35 -0500

    add overview section

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,11 +1,10 @@
-<template> 
-    <p class="h1-semibold">{{ $t('header.title') }}</p>
-</template> 
+<template>
+  <p class="h1-semibold">{{ $t("header.title") }}</p>
+  <OverviewSection />
+</template>
 
-<script lang='ts' setup> 
+<script lang="ts" setup>
+import OverviewSection from "./sections/OverviewSection.vue";
+</script>
 
-</script> 
-
-<style lang='scss' scoped> 
-
-</style>
+<style lang="scss" scoped></style>
